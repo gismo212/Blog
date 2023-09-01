c1ab6219da86fb4ff82fc6aecfb32969f1242d55
Elegard <whiteandy21321@gmail.com>
2023-09-01 22:58:38 +0300

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe Article do
-  it { should have_many :comments } 
+  describe "validation" do
+    it { should validate_presence_of :text}
+    it { should validate_presence_of :title} 
+  end
+
+  describe "associations" do
+    it { should have_many :comments }
+  end
 end
