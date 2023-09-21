44f9538f1acae66cbe8857325a23e7c392eb0e32
Elegard <whiteandy21321@gmail.com>
2023-09-21 18:06:27 +0300

    create validation length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, length: {maximum: 140}
+  validates :text, length:  {maximum: 4000}
   has_many :comments
 
   def subject
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :body ,length: {maximum: 4000}
 end
